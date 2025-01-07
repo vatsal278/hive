401d1670544dca6f7ad6fe7996c7da9279d34055
Vatsal Chaturvedi <vatsalchaturvedi278@gmail.com>
2025-01-07 13:13:24 +0000

    FDBDF

--- main.py
+++ main.py
@@ -350,6 +350,5 @@ if __name__ == "__main__":
         host="0.0.0.0", 
         port=5000,
         debug=False,
-        use_reloader=False,
-        allow_unsafe_werkzeug=True
+        use_reloader=False
     )
